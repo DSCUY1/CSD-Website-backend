553a2d83e42939f34bd65d6b5284399529c93498
pantaLuc <lucapameni@gmail.com>
2021-03-09 02:17:04 +0100

    creation du docker file et docker compose

--- CSDWebsitebackend/settings.py
+++ CSDWebsitebackend/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'CSDWebsitebackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'dbcsdwebsite',
+        'PORT': '3306'
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ COPY  requirements.txt /app/requirements.txt
 RUN pip install -r requirements.txt
 COPY . /app
 
-CMD python manage.py runserver 0.0.0.0:6000
+CMD python manage.py runserver 0.0.0.0:7000
