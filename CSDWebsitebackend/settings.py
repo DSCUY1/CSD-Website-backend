92a1610b4d33d08d240354348f2586044ec69e1d
Abdel Mfossa <abdelemfossa@gmail.com>
2021-03-24 20:50:46 +0100

    Ajout du .gitignore et export du SECRET_KEY dans .env

--- CSDWebsitebackend/settings.py
+++ CSDWebsitebackend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+14#*yk@4lhdwd-4e5b%$n0nf2s1_laf^uzq)@g7lluyc0bbcs'
+SECRET_KEY = os.environ.get('SECRET_KEY',)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
